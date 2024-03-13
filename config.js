df63ae882f5f7f724db72d4e81bd97c503f74f31
Pavani KUnapureddy <pavani1907@gmail.com>
2024-03-13 12:33:50 -0500

    my project

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 // eslint-disable-next-line no-undef
 module.exports = {
-    API_URL: "https://cnt-8b7467ff-be68-478f-b604-ec833132e67c.containerhub.tripleten-services.com"
+    API_URL: "https://cnt-f63f95dd-efea-427a-84b1-70778ae2fa1d.containerhub.tripleten-services.com"
 
 }
